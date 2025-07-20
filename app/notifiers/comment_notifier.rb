beaa2a916fedd65f46caa5613cdbb76f69fb44e0
Felipe Henrique <felipehhenriquee@gmail.com>
2025-07-20 17:39:38 -0300

    first commit

--- app/notifiers/comment_notifier.rb
+++ app/notifiers/comment_notifier.rb
@@ -7,11 +7,8 @@ class CommentNotifier < ApplicationNotifier
   #
   # deliver_by :email do |config|
   #   config.mailer = "UserMailer"
-  #   config.method = "new_comment_email"
-  # config.params = ->{ params }
-  # config.args = ->{ [1, 2, 3] }
+  #   config.method = "new_post"
   # end
-
   #
   # bulk_deliver_by :slack do |config|
   #   config.url = -> { Rails.application.credentials.slack_webhook_url }
@@ -24,20 +21,4 @@ class CommentNotifier < ApplicationNotifier
   # Add required params
   #
   # required_param :message
-  # Define helper methods to make rendering easier.
-  def message
-    # test
-    # @post = CommentNotifier.find(id).params[:post]
-    # @user = User.find((CommentNotifier.find(id).params[:post].user_id))
-    @post = record.post
-    @user = record.user
-    "#{@user.name} replied to #{@post.title.truncate(14)}"
-  end
-
-  # That allows us to do our url construction
-  def url
-    # test
-    # post_path(CommentNotifier.find(id).params[:post].id)
-    post_path(record.post.id)
-  end
 end
